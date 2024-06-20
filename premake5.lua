0be19f4abc8dd4b86de5b293603f9837387b0897
Plomek1 <118308483+Plomek1@users.noreply.github.com>
2024-06-20 17:54:59 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -9,14 +9,21 @@ project "GLFW"
 	files
   	{
     		"include/GLFW/glfw3.h",
-    		"include/GLFW/glfw3native.h",
-    		"src/glfw_config.h",
-    		"src/context.h",
-    		"src/init.h",
-    		"src/input.h",
-    		"src/monitor.h",
-    		"src/vulkan.h",
-    		"src/window.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
   	}
 
   	filter "system:windows"
@@ -26,14 +33,15 @@ project "GLFW"
     		files
     		{
       			"src/win32_init.c",
-      			"src/win32_joystick.c",
-      			"src/win32_monitor.c",
-      			"src/win32_time.c",
-      			"src/win32_thread.c",
-     			"src/win32_window.c",
-      			"src/wgl_context.c",
-      			"src/egl_context.c",
-      			"src/osmesa_context.c"
+			"src/win32_joystick.c",
+			"src/win32_module.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
     		}
 
     		defines
@@ -41,6 +49,7 @@ project "GLFW"
       			"_GLFW_WIN32",
       			"_CRT_SECURE_NO_WARNINGS"
     		}
+
 	filter "configurations:Debug"
 		runtime "Debug"
 
