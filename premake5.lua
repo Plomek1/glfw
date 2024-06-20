308fac4073d94cdc5ef4bfb0a4915f9a9ad9502e
Plomek1 <118308483+Plomek1@users.noreply.github.com>
2024-06-20 17:48:10 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "Off"	
 
 	targetdir("bin/" .. outputdir .. "/%{prj.name}")
 	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -21,7 +22,6 @@ project "GLFW"
   	filter "system:windows"
     		buildoptions { "-std=c11", "-lgdi32" }
     		systemversion "latest"
-    		staticruntime "On"
 
     		files
     		{
@@ -41,3 +41,11 @@ project "GLFW"
       			"_GLFW_WIN32",
       			"_CRT_SECURE_NO_WARNINGS"
     		}
+	filter "configuration:Debug"
+		runtime "Debug"
+
+	filter "configuration:Release"
+		runtime "Release"
+
+	filter "configuration:Dist"
+		runtime "Release"	
